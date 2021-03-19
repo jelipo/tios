f5569baac85a3f4b2f7faca4c571e963d3bf4a4d
jelipo <me@jelipo.com>
2021-03-19 09:48:30 +0800

    try risc-v

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+extern "C" fn _start() {
+    loop {};
 }
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
