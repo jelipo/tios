93adcfe528810e7bb2d6f33ebaef80d8239c61df
jelipo <me@jelipo.com>
2021-04-02 18:40:49 +0800

    update system

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,25 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-extern "C" fn _start() {
-    loop {};
-}
+use crate::sbi::sbi_call;
+
+mod console;
+mod sbi;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+const SBI_SHUTDOWN: usize = 8;
 
-const SYSCALL_EXIT: usize = 93;
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        llvm_asm!("ecall"
-            : "={x10}" (ret)
-            : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
-            : "memory"
-            : "volatile"
-        );
-    }
-    ret
+pub fn shutdown() -> ! {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    panic!("It should shutdown!");
 }
 
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+#[no_mangle]
+extern "C" fn _start() {
+    shutdown();
 }
+
