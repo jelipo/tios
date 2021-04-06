eb5352db3b4f43e46c5a15b6e322bc973f7e1754
jelipo <me@jelipo.com>
2021-04-06 18:31:58 +0800

    update system

---
+++ exchange_bin.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# init
+project_dir=$(dirname "$0")
+bin_dir=$project_dir/target/riscv64gc-unknown-none-elf/release/
+echo "Set path = $bin_dir"
+#
+rust-objcopy --binary-architecture=riscv64 $bin_dir/tios --strip-all -O binary $bin_dir/tios.bin
+#
+echo done.
